e8b3bb79fe2bfacb07eadf4ac808c3882ea925f8
Zak Farrow <zak.farrow1999@gmail.com>
2024-07-31 17:09:18 +0100

    feature: auto scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import Hero from "@/components/Hero"
-import Navbar from "@/components/Navbar"
+"use client";
 
-export default function Home() {
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import React, { useRef } from "react";
+
+const Home: React.FC = () => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = () => {
+    sectionRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="flex flex-col">
       <Navbar />
       <Hero />
+      <button onClick={scrollToSection}>More</button>
+      <div ref={sectionRef} className="h-screen flex flex-col items-center justify-center">
+        <h1>Secton 1</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+          tempora. Earum magni quis excepturi nisi. Excepturi, ab voluptatum
+          itaque tenetur magnam soluta optio nemo id expedita dolorem
+          accusantium, veritatis odio.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
