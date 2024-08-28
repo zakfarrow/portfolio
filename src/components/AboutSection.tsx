1dc7ff73e36971bf367f54b02fd31649ef10b2c5
zfarrow <zakf@air-charge.com>
2024-08-28 09:57:50 +0100

    updated about me section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,34 +1,23 @@
 const AboutSection = () => {
   return (
-    <section id="about" className="scroll-mt-16 mb-16 flex flex-col gap-y-8">
-      <h1>About Me</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
-        consequuntur nisi, maxime dolor, molestias dignissimos totam enim
-        accusantium amet doloribus sit soluta cum modi ullam sed ad,
-        necessitatibus deserunt atque.Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Optio consequuntur nisi, maxime dolor, molestias
-        dignissimos totam enim accusantium amet doloribus sit soluta cum modi
-        ullam sed ad, necessitatibus deserunt atque.
+    <section id="about" className="mb-16 flex scroll-mt-16 flex-col">
+      <h1 className="py-8">About Me</h1>
+      <p className="py-4">
+        Hi, I&apos;m Zak Farrow, a software engineer who loves turning complex
+        ideas into smooth, user-friendly applications. With a strong background
+        in Mathematics and Computer Science, I specialize in React and Python
+        but enjoy experimenting with a wide range of technologies. Whether
+        it&apos;s building full-stack web applications or optimizing
+        performance, I&apos;m all about crafting clean, maintainable code that
+        just works.
       </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
-        consequuntur nisi, maxime dolor, molestias dignissimos totam enim
-        accusantium amet doloribus sit soluta cum modi ullam sed ad,
-        necessitatibus deserunt atque.Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Optio consequuntur nisi, maxime dolor, molestias
-        dignissimos totam enim accusantium amet doloribus sit soluta cum modi
-        ullam sed ad, necessitatibus deserunt atque.
+      <p className="py-4">
+        Currently, I&apos;m part of the team at Aircharge, where we&apos;re
+        creating innovative IoT-connected wireless charging solutions. In my
+        downtime, you might find me working on my side projects, learning a new
+        technology, or brainstorming the next big project.
       </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
-        consequuntur nisi, maxime dolor, molestias dignissimos totam enim
-        accusantium amet doloribus sit soluta cum modi ullam sed ad,
-        necessitatibus deserunt atque.Lorem, ipsum dolor sit amet consectetur
-        adipisicing elit. Optio consequuntur nisi, maxime dolor, molestias
-        dignissimos totam enim accusantium amet doloribus sit soluta cum modi
-        ullam sed ad, necessitatibus deserunt atque.
-      </p>
+      <p className="py-4">Let&apos;s connect and create something amazing!</p>
     </section>
   );
 };
